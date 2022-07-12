d67ed2ce086462575cc3dbc3100420f9e157c0fc
Esau21 <80347597+Esau21@users.noreply.github.com>
2022-07-11 19:40:51 -0600

    codigo

---
+++ database/migrations/2022_05_03_034431_create_detalle_ingresos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetalleIngresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_ingresos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idingreso')->unsigned();
+            $table->foreign('idingreso')->references('id')->on('ingresos')->onDelete('cascade');
+            $table->integer('idinventarios')->unsigned();
+            $table->foreign('idinventarios')->references('id')->on('inventarios');
+            $table->integer('cantidad');
+            $table->decimal('precio', 11, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalle_ingresos');
+    }
+}
